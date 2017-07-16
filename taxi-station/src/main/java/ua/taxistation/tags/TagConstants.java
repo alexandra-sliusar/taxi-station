73e1c88db224f231c9691c151970043e333fde8b
alexandra-sliusar <alexalioness@gmail.com>
2017-07-16 22:52:11 +0300

    added custom tags with java classes, commands for drivers, dispatchers and clients

---
+++ taxi-station/src/main/java/ua/taxistation/tags/TagConstants.java
@@ -0,0 +1,20 @@
+package ua.taxistation.tags;
+
+public class TagConstants {
+	public static String tableStartTag = "<table class=\"table\">";
+	public static String tableEndTag = "</table>";
+	public static String tableHeadStartTag = "<thead>";
+	public static String tableHeadEndTag = "</thead>";
+	public static String tableRowStartTag = "<tr>";
+	public static String tableRowEndTag = "</tr>";
+	public static String tableHeaderCellStartTag = "<th>";
+	public static String tableHeaderCellEndTag = "</th>";
+	public static String tableBodyStartTag = "<tbody>";
+	public static String tableBodyEndTag = "</tbody>";
+	public static String tableCellStartTag = "<td>";
+	public static String tableCellEndTag = "</td>";
+	public static String nextLineTag = "<br />";
+	public static String linkToRequestStartTag = "<a href=\"./requests/request?req_id=";
+	public static String linkToRequestEnd = "\">";
+	public static String linkEndTag = "</a>";
+}
