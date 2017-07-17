56734d7e60cbee02371f8cfd6280e19a6fce95a9
alexandra-sliusar <alexalioness@gmail.com>
2017-07-17 18:03:34 +0300

    added filter and error page

---
+++ taxi-station/src/main/java/ua/taxistation/controller/filters/IllegalAccessFilter.java
@@ -0,0 +1,80 @@
+package ua.taxistation.controller.filters;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import ua.taxistation.controller.constants.Page;
+import ua.taxistation.controller.constants.Parameters;
+import ua.taxistation.controller.utilities.RedirectionManager;
+import ua.taxistation.entity.User;
+import ua.taxistation.entity.enums.Role;
+import ua.taxistation.utilities.LocaleManager;
+
+@WebFilter(urlPatterns = { "/main/client/*", "/main/driver/*", "/main/dispatcher/*" })
+public class IllegalAccessFilter implements Filter {
+
+	private final static Logger LOGGER = Logger.getLogger(IllegalAccessFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+		HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+		User user = (User) httpRequest.getSession().getAttribute(Parameters.USER);
+
+		if (!isUserSignIn(user) || !isPathLegalForUser(httpRequest.getRequestURI(), user)) {
+			LOGGER.info("Unauthorized access to the resource: " + httpRequest.getRequestURI());
+			Map<String, String> message = new HashMap<>();
+			message.put(Parameters.ERROR, LocaleManager.BUNDLE.getString(LocaleManager.ILLEGAL_ACCESS));
+			RedirectionManager.getInstance().redirectWithMessages(httpRequest, httpResponse, Page.MAIN_PATH, message);
+			return;
+		}
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	private boolean isUserSignIn(User user) {
+		return user != null;
+
+	}
+
+	private boolean isPathLegalForUser(String servletPath, User user) {
+		return (isDriverPage(servletPath) && user.getRole().equals(Role.DRIVER))
+				|| (isDispatcherPage(servletPath) && user.getRole().equals(Role.DISPATCHER))
+				|| (isClientPage(servletPath) && user.getRole().equals(Role.CLIENT));
+	}
+
+	private boolean isDriverPage(String requestURI) {
+		return requestURI.contains(Role.DRIVER.name().toLowerCase());
+	}
+
+	private boolean isDispatcherPage(String requestURI) {
+		return requestURI.contains(Role.DISPATCHER.name().toLowerCase());
+	}
+
+	private boolean isClientPage(String requestURI) {
+		return requestURI.contains(Role.CLIENT.name().toLowerCase());
+	}
+
+}
---
+++ taxi-station/src/main/webapp/WEB-INF/pages/pagenotfound.jsp
@@ -0,0 +1,39 @@
+<%@ page isErrorPage="true" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+
+
+<c:set var="lang" scope="session" value="${empty sessionScope.locale ? 'en_GB' : sessionScope.locale}" />
+<fmt:setLocale value="${lang}" scope="session" />
+<fmt:setBundle basename="/i18n/messages" var="bundle" scope="session" />
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Page not found</title>
+
+<link rel="stylesheet" type="text/css"
+	href="<c:url value="/resources/css/style.css" />" />
+<link rel="stylesheet" type="text/css"
+	href="<c:url value="/resources/bootstrap-3.3.7-dist/css/bootstrap.min.css" />" />
+</head>
+<body>
+	<div class="container-fluid " align="center">
+		<div class="row">
+
+			<div class="error alert alert-danger">
+				<strong> ${pageContext.errorData.statusCode}</strong><br /> <strong>
+					<fmt:message key="taxistation.error.pagenotfound" bundle="${bundle}" />
+				</strong>
+			</div>
+
+		</div>
+		<div class="row-fluid">
+			<a href="${pageContext.request.contextPath}/main/"><fmt:message key="taxistation.error.backtohomepage" bundle="${bundle}" /></a>
+		</div>
+
+	</div>
+</body>
+</html>
